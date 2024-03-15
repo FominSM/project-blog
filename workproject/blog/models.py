dc9e851ea6a4ee0ae357935692dca73924af45ce
FominS <fomin-sm1998@mail.ru>
2024-03-15 16:39:01 +0300

    Тестовая БД +

--- workproject/blog/models.py
+++ workproject/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -16,7 +17,7 @@ class Post(models.Model):
 
 
     title = models. CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish') # при использовании параметра unique_for_date поле slug должно быть уникальным для даты, сохраненной в поле publish
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts') #related_name, чтобы указывать имя обратной связи, от User к Post - user.blog_posts
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -35,3 +36,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # будет формировать URL-адрес динамически, применяя имя URL-адреса, определенное в  шаблонах URL-адресов
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
