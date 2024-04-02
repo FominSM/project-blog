6a40ba1d728868b29331c416fed33918e7364414
Sergey Fomin <fomin-sm1998@mail.ru>
2024-04-02 21:23:07 +0300

    Функционал поиска по сайту завершен

--- workproject/blog/views.py
+++ workproject/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm, SearchForm
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -110,11 +110,15 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
+    
+    print(request.GET)
 
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
     
     return render(request, 'blog/post/search.html', {'form': form,'query': query,'results': results})
